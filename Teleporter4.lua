d872c0fb83ae381ffb040979cfaeef035776517d
Gard Hull <gard@bitwizardry.com>
2015-07-15 23:28:29 -0400

    Right pane displaying with test data

--- Teleporter4.lua
+++ Teleporter4.lua
@@ -5,7 +5,6 @@ local Tp4Class = ZO_Object:Subclass()
 local Tp4=Tp4Class:New()
 
 Tp4.addonName = "Teleporter4"
--- Tp4.testvar = GetUnitName("player")
 Tp4.chatWasMinimized = false
 Tp4.defaults = { }
 
@@ -13,8 +12,15 @@ ZO_CreateStringId("TP4_NAME", "Teleporter4")
 
 local Tp4_TLW = nil
 local TP4_RIGHTPANE = nil
+local TP4_LOCATION_DATA = 1
+local playerData = {"Dummyplayer", "dummyzone"}
+local TP4_ScrollList_SORT_KEYS =
+{
+    ["playerName"] = { },
+    ["zoneName"] = {  tiebreaker = "playerName" },
+}
 
-
+--[[
 local function showTp4Windows(toggle)
     --- Tp4_LootWindow:SetHidden( not toggle )
     --- Tp4_Map:SetHidden( not toggle )
@@ -25,7 +31,19 @@ local function showTp4Windows(toggle)
         end
     end
 end
+--]]
+local function populateScrollList()
+    local scrollData = ZO_ScrollList_GetDataList(TP4_RIGHTPANE.ScrollList)
+    ZO_ClearNumericallyIndexedTable(scrollData)
 
+    table.insert(scrollData, ZO_ScrollList_CreateDataEntry(TP4_LOCATION_DATA,
+        {
+            playerName = "Test Name",
+            zoneName = "Test Zone"
+        }
+        )
+    )
+end
 
 local function createTp4RightPane()
 	local x,y = ZO_WorldMapLocations:GetDimensions()
@@ -46,106 +64,72 @@ local function createTp4RightPane()
     TP4_RIGHTPANE.Headers:SetAnchor( TOPLEFT, TP4_RIGHTPANE, TOPLEFT, 0, 0 )
     TP4_RIGHTPANE.Headers:SetHeight(32)
 
-    TP4_RIGHTPANE.Headers.Name = WINDOW_MANAGER:CreateControlFromVirtual("$(parent)Name",TP4_RIGHTPANE.Headers,"ZO_SortHeaderIcon")
-    TP4_RIGHTPANE.Headers.Name:SetDimensions(70,32)
+    TP4_RIGHTPANE.Headers.Name = WINDOW_MANAGER:CreateControlFromVirtual("$(parent)Name",TP4_RIGHTPANE.Headers,"ZO_SortHeader")
+    TP4_RIGHTPANE.Headers.Name:SetDimensions(115,32)
     TP4_RIGHTPANE.Headers.Name:SetAnchor( TOPLEFT, TP4_RIGHTPANE.Headers, TOPLEFT, 8, 0 )
-    ZO_SortHeader_InitializeArrowHeader(TP4_RIGHTPANE.Headers.Name, "nameHeader", ZO_SORT_ORDER_UP)
-    ZO_SortHeader_SetTooltip(TP4_RIGHTPANE.Headers.Name, "Sort on Name")
+    ZO_SortHeader_Initialize(TP4_RIGHTPANE.Headers.Name, "Name", "playerName", ZO_SORT_ORDER_UP, TEXT_ALIGN_LEFT, "ZoFontGameLargeBold")
+    ZO_SortHeader_SetTooltip(TP4_RIGHTPANE.Headers.Name, "Sort on player name")
 
     TP4_RIGHTPANE.Headers.Location = WINDOW_MANAGER:CreateControlFromVirtual("$(parent)Location",TP4_RIGHTPANE.Headers,"ZO_SortHeader")
-    TP4_RIGHTPANE.Headers.Location:SetDimensions(160,32)
+    TP4_RIGHTPANE.Headers.Location:SetDimensions(115,32)
     TP4_RIGHTPANE.Headers.Location:SetAnchor( LEFT, TP4_RIGHTPANE.Headers.Name, RIGHT, 18, 0 )
-    ZO_SortHeader_Initialize(TP4_RIGHTPANE.Headers.Location, "Location List", "locationName", ZO_SORT_ORDER_UP, TEXT_ALIGN_LEFT, "ZoFontGameLargeBold")
-    ZO_SortHeader_SetTooltip(TP4_RIGHTPANE.Headers.Location, "Sort on Location")
+    ZO_SortHeader_Initialize(TP4_RIGHTPANE.Headers.Location, "Zone", "zoneName", ZO_SORT_ORDER_UP, TEXT_ALIGN_LEFT, "ZoFontGameLargeBold")
+    ZO_SortHeader_SetTooltip(TP4_RIGHTPANE.Headers.Location, "Sort on zone")
 
     local sortHeaders = ZO_SortHeaderGroup:New(TP4_RIGHTPANE:GetNamedChild("Headers"), SHOW_ARROWS)
+    sortHeaders:RegisterCallback(
+        ZO_SortHeaderGroup.HEADER_CLICKED,
+        function(key, order)
+            table.sort(
+                ZO_ScrollList_GetDataList(TP4_RIGHTPANE.ScrollList),
+                function(entry1, entry2)
+                    return ZO_TableOrderingFunction(entry1.data, entry2.data, key, TP4_ScrollList_SORT_KEYS, order)
+                end)
+
+            ZO_ScrollList_Commit(TP4_RIGHTPANE.ScrollList)
+        end)
+    sortHeaders:AddHeadersFromContainer()
+
+    -- Create a scrollList
+    TP4_RIGHTPANE.ScrollList = WINDOW_MANAGER:CreateControlFromVirtual("$(parent)Tp4ScrollList", TP4_RIGHTPANE, "ZO_ScrollList")
+    TP4_RIGHTPANE.ScrollList:SetDimensions(x, y-32)
+    TP4_RIGHTPANE.ScrollList:SetAnchor(TOPLEFT, TP4_RIGHTPANE.Headers, BOTTOMLEFT, 0, 0)
+
+    --
+    -- Add a datatype to the scrollList
+    --
+    ZO_ScrollList_Initialize(TP4_RIGHTPANE.ScrollList)
+    ZO_ScrollList_EnableHighlight(TP4_RIGHTPANE.ScrollList, "ZO_ThinListHighlight")
+    ZO_ScrollList_AddDataType(TP4_RIGHTPANE.ScrollList, TP4_LOCATION_DATA, "Tp4Row", 23,
+        function(control, data)
+            control:GetNamedChild("Name"):SetText(data.playerName)
+            control:GetNamedChild("Location"):SetText(data.zoneName)
+        end
+    )
+
+    populateScrollList()
 
     local buttonData = {
-		normal = "EsoUI/Art/mainmenu/menubar_journal_up.dds",
-		pressed = "EsoUI/Art/mainmenu/menubar_journal_down.dds",
-		highlight = "EsoUI/Art/mainmenu/menubar_journal_over.dds",                  
-	}
-	--
+        normal = "EsoUI/Art/mainmenu/menubar_journal_up.dds",
+        pressed = "EsoUI/Art/mainmenu/menubar_journal_down.dds",
+        highlight = "EsoUI/Art/mainmenu/menubar_journal_over.dds",
+    }
+
+    --
 	-- Create a fragment from the window and add it to the modeBar of the WorldMap RightPane
 	--
-	local tp4Fragment = ZO_FadeSceneFragment:New(TP4_RIGHTPANE) 
+	local tp4Fragment = ZO_FadeSceneFragment:New(TP4_RIGHTPANE)
 	WORLD_MAP_INFO.modeBar:Add(TP4_NAME, {tp4Fragment}, buttonData)
+
     --
     -- Create a few preHookHandlers so that the map shows up back again when hidden
     --     
-    ZO_PreHookHandler(TP4_RIGHTPANE     , "OnHide", function() showTp4Windows( false ) end)
-    ZO_PreHookHandler(ZO_WorldMapLocations, "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
-    ZO_PreHookHandler(ZO_WorldMapFilters  , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
-    ZO_PreHookHandler(ZO_WorldMapKey      , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
-    ZO_PreHookHandler(ZO_WorldMapQuests   , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
-    ZO_PreHookHandler(TP4_RIGHTPANE     , "OnShow", function() Tp4.chatWasMinimized = CHAT_SYSTEM:IsMinimized() end)
-end
-
-
-local function createTp4Interface()
-    --
-    -- This object should hold everything ...
-    --
----    local x,y = ZO_WorldMap:GetDimensions()
----    x = x + 500
----    y = y + 80
-
-    Tp4_TLW = WINDOW_MANAGER:CreateTopLevelWindow(nil)
-    Tp4_TLW:SetMovable( false )
-    Tp4_TLW:SetClampedToScreen(true)
-    Tp4_TLW:SetDimensions( x , y )
-    Tp4_TLW:SetAnchor( TOPRIGHT, ZO_WorldMap, TOPRIGHT, 40, -40 )
-    Tp4_TLW:SetHidden( true )
-    --
-    -- Define a label that holds the addonName and show it on top of the TLW
-    --
-    Tp4_TLW.title = WINDOW_MANAGER:CreateControl(nil, Tp4_TLW, CT_LABEL)
-    Tp4_TLW.title:SetColor(0.8, 0.8, 0.8, 1)
-    Tp4_TLW.title:SetFont("ZoFontAlert")
-    Tp4_TLW.title:SetScale(1.5)
-    Tp4_TLW.title:SetWrapMode(TEX_MODE_CLAMP)
-    Tp4_TLW.title:SetDrawLayer(2)
-    Tp4_TLW.title:SetText("Teleporter4")
-    Tp4_TLW.title:SetAnchor(TOP, Tp4_TLW, nil, 110, -10)
-    Tp4_TLW.title:SetDimensions(200,25)
-    --
----    -- Define a texture that holds the faction of the dungeon
----    --
----    Tp4_TLW.factionTextureLeft = WINDOW_MANAGER:CreateControl(nil, Tp4_TLW, CT_TEXTURE)
----    Tp4_TLW.factionTextureLeft:SetDimensions(x-240,y-500)
----    Tp4_TLW.factionTextureLeft:SetAnchor(TOPLEFT, Tp4_TLW, TOPLEFT, 40, 0)
----
----    Tp4_TLW.factionTextureRight = WINDOW_MANAGER:CreateControl(nil, Tp4_TLW, CT_TEXTURE)
----    Tp4_TLW.factionTextureRight:SetDimensions(400,y-500)
----    Tp4_TLW.factionTextureRight:SetAnchor(TOPRIGHT, Tp4_TLW, TOPRIGHT, 200, 0)
----    --
----    -- Define a divider above the faction textures
----    --
----    Tp4_TLW.titledividerLeft = WINDOW_MANAGER:CreateControl(nil,  Tp4_TLW, CT_TEXTURE)
----    Tp4_TLW.titledividerLeft:SetDimensions(x-100,4)
----    Tp4_TLW.titledividerLeft:SetAnchor(TOPLEFT, Tp4_TLW, TOPLEFT, 50, 6)
----    Tp4_TLW.titledividerLeft:SetTexture("/esoui/art/guild/sectiondivider_left.dds")
----
----    Tp4_TLW.titledividerRight = WINDOW_MANAGER:CreateControl(nil,  Tp4_TLW, CT_TEXTURE)
----    Tp4_TLW.titledividerRight:SetDimensions(100,4)
----    Tp4_TLW.titledividerRight:SetAnchor(TOPLEFT, Tp4_TLW.titledividerLeft, TOPRIGHT, 0, 0)
----    Tp4_TLW.titledividerRight:SetTextureCoords(0, 1, 0, 0.391)
----    Tp4_TLW.titledividerRight:SetTexture("/esoui/art/guild/sectiondivider_right.dds")
----    --
----    -- Add a faction texture
----    --
----    Tp4_TLW.NameHeaderIconTexture = WINDOW_MANAGER:CreateControl(nil,  Tp4_TLW, CT_TEXTURE)
----    Tp4_TLW.NameHeaderIconTexture:SetDimensions(120,120)
----    Tp4_TLW.NameHeaderIconTexture:SetAnchor(TOP, Tp4_TLW, nil, (x/2)-140, 50)
----    Tp4_TLW.factionTextureLeft:SetTexture("/esoui/art/campaign/overview_scoringbg_daggerfall_left.dds")
----    Tp4_TLW.factionTextureRight:SetTexture("/esoui/art/campaign/overview_scoringbg_daggerfall_right.dds")
----    Tp4_TLW.NameHeaderIconTexture:SetTexture("/esoui/art/compass/ava_borderkeep_pin_daggerfall.dds")
----    Tp4_TLW.NameHeaderIconTexture:SetHidden(false)
-
----    Tp4_TLW.NameHeaderIconTexture:SetTexture("")	--Will be set when zone gets determined
-
-
-    createTp4RightPane()
+    --ZO_PreHookHandler(TP4_RIGHTPANE     , "OnHide", function() showTp4Windows( false ) end)
+    --ZO_PreHookHandler(ZO_WorldMapLocations, "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
+    --ZO_PreHookHandler(ZO_WorldMapFilters  , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
+    --ZO_PreHookHandler(ZO_WorldMapKey      , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
+    --ZO_PreHookHandler(ZO_WorldMapQuests   , "OnShow", function() ZO_WorldMap:SetHidden( false ) showTp4Windows( false ) end)
+    --ZO_PreHookHandler(TP4_RIGHTPANE     , "OnShow", function() Tp4.chatWasMinimized = CHAT_SYSTEM:IsMinimized() end)
 end
 
 
@@ -160,7 +144,7 @@ end
 function Tp4:EVENT_ADD_ON_LOADED(eventCode, addonName, ...)
     if addonName == Tp4.addonName then
         Tp4.SavedVariables = ZO_SavedVars:New("Tp4_SavedVariables", 2, nil, Tp4.defaults)
-        createTp4Interface()
+        createTp4RightPane()
 
 
         SLASH_COMMANDS["/tp4"] = processSlashCommands
